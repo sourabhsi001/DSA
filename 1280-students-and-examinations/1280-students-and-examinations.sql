40375af78593b990cd53f20efaece30d9fb2a1f7
sourabhsi001 <160339718+sourabhsi001@users.noreply.github.com>
2026-09-11 23:00:15 +0530

    Time: 1342 ms (19.46%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,13 +1,9 @@
 # Write your MySQL query statement below
-select s.student_id,s.student_name,sub.subject_name,count(e.subject_name) as attended_exams
+select s.student_id,s.student_name,s1.subject_name,count(e.subject_name)as attended_exams
 from students as s
-cross join subjects as sub
-left join examinations as e
+cross join subjects as s1
+left join examinations e
 on s.student_id=e.student_id
-and sub.subject_name=e.subject_name
-group by 
-    student_id,student_name,subject_name
-order by
-    student_id,
-    student_name,
-    subject_name;
+and s1.subject_name=e.subject_name
+group by student_id,student_name,subject_name
+order by student_id,s.student_name,s1.subject_name;
