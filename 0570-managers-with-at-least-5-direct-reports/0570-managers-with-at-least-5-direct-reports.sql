e8d3724e3e5cf4a642eb59111c4d2932e3e2787c
sourabhsi001 <160339718+sourabhsi001@users.noreply.github.com>
2026-09-21 14:46:29 +0530

    Time: 325 ms (97.51%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,17 @@
 # Write your MySQL query statement below
-select e2.name 
-from employee as e1
-join employee as e2
-on e1.managerid=e2.id
-group by e2.id,e2.name
-having count(*)>=5;
+# Write your MySQL query statement below
+-- select e1.name
+-- from employee e1
+-- join employee e2
+-- on e1.id=e2.managerId
+-- group by e1.name
+-- having count(*)>=5;
+SELECT NAME 
+FROM EMPLOYEE
+WHERE ID in (SELECT managerID
+            from employee 
+            group by managerid
+            having count(managerId)>=5
+                            
+                            );
+
